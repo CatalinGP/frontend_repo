13d4d561c8496e10aebcef790c96186f8adac973
andref00 <andreea.fertu@randstaddigital.com>
2024-11-14 12:03:05 +0200

    Integrate API Manager for doors

--- carsdata/src/components/UDScomponents/DivCenterDoors.tsx
+++ carsdata/src/components/UDScomponents/DivCenterDoors.tsx
@@ -5,6 +5,7 @@ import ModalUDS from './ModalUDS';
 import './style.css';
 import { displayLoadingCircle, displayErrorPopup, removeLoadingCicle } from '../sharedComponents/LoadingCircle';
 import logger from '@/src/utils/Logger';
+import { Endpoints, apiManager, DoorsItems } from '@/src/utils/ApiManager';
 
 export interface doorsData {
     ajar: any,
@@ -24,131 +25,174 @@ const checkInput = (message: any) => {
     return value;
 };
 
-export const writeInfoDoors = async (setData: any) => {
-    displayLoadingCircle();
-    const door = checkInput('Enter Door Parameter:');
-    const passenger = checkInput('Enter Passenger:');
-    const passenger_lock = checkInput('Enter Passenger Lock:');
-    const driver = checkInput('Enter Driver:');
-    const ajar = checkInput('Enter Ajar:');
-    const is_manual_flow = true;
+export const readInfoDoors = async (setData: any,
+    options: { manual_flow?: boolean; item?: DoorsItems | null } = {}) => {
 
-    const data = {
-        door: door || null,
-        passenger: passenger || null,
-        passenger_lock: passenger_lock || null,
-        driver: driver || null,
-        ajar: ajar || null,
-        is_manual_flow: is_manual_flow || null,
+    /* Set default values using destructuring for the options that were not provided */
+    const { manual_flow = false, item = null } = options;
 
-    };
-    console.log("Writing info doors...");
-    console.log(data);
-    try {
-        await fetch(`http://127.0.0.1:5000/api/write_info_doors`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        }).then(response => response.json())
-            .then(data => {
-                setData(data);
-                console.log(data);
-                // fetchLogs();
-            });
-    } catch (error) {
-        console.log(error);
-        removeLoadingCicle();
-    }
-    removeLoadingCicle();
-
-    readInfoDoors(false, setData)
-}
-
-export const readInfoDoors = async (isManualFlow:boolean, setData:any) => {
     displayLoadingCircle();
     console.log("Reading doors info...");
+
     try {
-        await fetch(`http://127.0.0.1:5000/api/read_info_doors?is_manual_flow=${isManualFlow}`, {
-            method: 'GET',
-        }).then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData(data);
-                if (data?.ERROR === "interrupted") {
-                    displayErrorPopup("Connection failed");
-                }
-            });
+        /* Use API Manager to do the API call */
+        const data = await apiManager.apiCall(Endpoints.READ_DOORS, {
+            manual_flow: options.manual_flow,
+            item: options.item
+        });
+
+        console.log("Read doors data: ", data);
+        setData(data);
+
+        /* If communication was intrerrupted, log error */
+        if (data?.ERROR === 'interrupted') {
+            console.error("Connection interrupted");
+            displayErrorPopup("Connection failed");
+        }
     } catch (error) {
-        console.log(error);
-        removeLoadingCicle();
+        /* Handler for errors returned by API Manager */
+        console.error("Error during read operation: ", error);
         displayErrorPopup("Connection failed");
-    }
-    removeLoadingCicle();
+    } finally { removeLoadingCicle(); }
 };
+
+export const writeInfoDoors = async (item: DoorsItems, newValue: string, setData: any,
+    options: { manual_flow?: boolean } = {}) => {
+
+    /* Set default values using destructuring for the options that were not provided */
+    const { manual_flow = false } = options;
+
+    console.log("Writing doors info...");
+
+    /* Set a dictionary for easier mapping over the values */
+    const variableMapping: { [key: string]: any } = {
+        door: { is_manual_flow: manual_flow, door: parseInt(newValue) },
+        passenger: { is_manual_flow: manual_flow, passenger: parseInt(newValue) },
+        passenger_lock: { is_manual_flow: manual_flow, passenger_lock: parseInt(newValue) },
+        ajar: { is_manual_flow: manual_flow, ajar: parseInt(newValue) },
+    };
+
+    const data_sent = variableMapping[item];
+
+    if (!data_sent) {
+        console.error(`Invalid variable for write: ${item}`);
+        return;
+    }
+
+    console.log(data_sent);
+    displayLoadingCircle();
+
+    try {
+        /* Use API Manager to do the API call */
+        const response = await apiManager.apiCall(Endpoints.WRITE_DOORS, {
+            json: data_sent,
+            manual_flow: manual_flow,
+        });
+
+        /* If communication was intrerrupted, log error */
+        if (response?.ERROR === 'interrupted') {
+            console.error("Connection interrupted");
+            displayErrorPopup("Connection failed");
+        }
+
+        console.log(response);
+    } catch (error) {
+        console.error("Error during write operation: ", error);
+        displayErrorPopup("Connection failed");
+    } finally {
+        removeLoadingCicle();
+        readInfoDoors(setData, { manual_flow: manual_flow });
+    }
+}
 
 
 const DivCenterDoors = (props: any) => {
     logger.init();
     const [data, setData] = useState<doorsData | null>(null);
-    
+
 
     useEffect(() => {
-        
-        readInfoDoors(false, setData);
+        readInfoDoors(setData);
     }, []);
 
 
     return (
         <div className="w-[65%] flex h-screen bg-indigo-950 math-paper">
+            {/* Left section */}
             <div className="w-[35%] flex flex-col items-center">
                 <h3 className="text-white text-3xl">Vehicle Model</h3>
                 {/* <button className="btn" onClick={callApi}>Button{jsonResp.age}</button> */}
                 <div className="w-full h-full flex flex-col items-center justify-center">
 
-                    <div className="w-[30%] m-7 text-white grid justify-items-end">
-                        <label htmlFor="my_modal_3"
-                            className="inline-flex items-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
-                            {data?.ajar}
-                        </label>
-                        <ModalUDS id="my_modal_3" cardTitle={'Door status - Ajar'} />
-                        <p>Ajar</p>
+                    {/* Ajar */}
+                    <div className="w-[30%] m-7 grid justify-items-end">
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn m-1 text-white inline-flex items-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
+                                {data?.ajar}
+                            </div>
+                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                <li><a onClick={() => writeInfoDoors("ajar", "0", setData)}>No Warning</a></li>
+                                <li><a onClick={() => writeInfoDoors("ajar", "1", setData)}>Warning</a></li>
+                            </ul>
+                        </div>
+                        <p className="text-white">Ajar</p>
                     </div>
 
-                    <div className="w-[30%] m-7 text-white grid justify-center">
-                        <label htmlFor="my_modal_2"
-                            className="inline-flex items-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
-                            {data?.door}
-                        </label>
-                        <ModalUDS id="my_modal_2" cardTitle={'Door'} />
-                        <p>Door</p>
+                    {/* Door */}
+                    <div className="w-[30%] m-7 grid justify-items-end">
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn m-1 text-white inline-flex items-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
+                                {data?.door}
+                            </div>
+                            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                <li><a onClick={() => writeInfoDoors("door", "0", setData)}>Closed</a></li>
+                                <li><a onClick={() => writeInfoDoors("door", "1", setData)}>Open</a></li>
+                            </ul>
+                        </div>
+                        <p className="text-white">Door</p>
                     </div>
 
-                    <div className="w-[30%] m-7 text-white grid justify-items-end">
-                        <label htmlFor="my_modal_3"
-                            className="inline-flex items-center justify-center p-2 bg-green-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-green-700">
-                            {data?.passenger}
-                        </label>
-                        <ModalUDS id="my_modal_3" cardTitle={'Passenger'} />
-                        <p>Passenger</p>
-                    </div>
+
 
                 </div>
             </div>
+
+            {/* Middle Section - Image */}
             <div className="w-[30%] h-screen flex">
                 <Image src={props.image} alt="car sketch" width={400} height={204} className="invert" />
             </div>
 
+
+            {/* Right section */}
             <div className="w-[35%] flex flex-col items-center justify-center">
 
-                <div className="w-[30%] m-7 text-white">
-                    <label htmlFor="my_modal_4"
-                        className="inline-flex items-center justify-center p-2 bg-blue-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-blue-700">
-                        {data?.passenger_lock}
-                    </label>
-                    <ModalUDS id="my_modal_4" cardTitle={'Passenger lock'} />
-                    <p>Passenger lock</p>
+                {/* Passenger */}
+                <div className="w-[30%] m-7 grid justify-items-start">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn m-1 text-white inline-flex items-center justify-center p-2 bg-red-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-red-700">
+                            {data?.passenger}
+                        </div>
+                        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li><a onClick={() => writeInfoDoors("passenger", "0", setData)}>Closed</a></li>
+                            <li><a onClick={() => writeInfoDoors("passenger", "1", setData)}>Open</a></li>
+                        </ul>
+                    </div>
+                    <p className="text-white">Passenger</p>
+                </div>
+
+                {/* Passenger lock*/}
+                <div className="w-[30%] m-7 grid justify-items-start">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn m-1 text-white inline-flex items-center justify-center p-2 bg-green-500 rounded-full border-4 border-gray-700 transition duration-300 ease-in-out hover:bg-green-700">
+                            {data?.passenger_lock}
+                        </div>
+                        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li><a onClick={() => writeInfoDoors("passenger_lock", "0", setData)}>Locked</a></li>
+                            <li><a onClick={() => writeInfoDoors("passenger_lock", "1", setData)}>Unlocked</a></li>
+                        </ul>
+                        <p className="text-white">Passenger lock</p>
+                    </div>
+
                 </div>
             </div>
         </div>
